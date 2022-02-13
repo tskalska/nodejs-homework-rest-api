db9b5aa44902d9cd4c14541f82da2771bba53f59
tskalska <tatiana.skalska@gmail.com>
2022-02-13 20:04:08 +0100

    password to gitignore

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-// CKhJ5e7cy1eKZIcr - mongodb
-// mongodb+srv://Skate:CKhJ5e7cy1eKZIcr@cluster0.beqzf.mongodb.net/test
 
-// const mongoose = require('mongoose');
 
 const express = require('express');
 const logger = require('morgan');
@@ -19,15 +16,6 @@ app.use(cors());
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter);
-
-// const {DB_HOST, PORT = 3000} = process.env;
-
-// mongoose.connect(DB_HOST)
-//   .then(()=>app.listen(PORT))
-//   .catch(error=>{
-//     console.log(error.message);
-//     process.exit(1); 
-//   });
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
